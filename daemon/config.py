49f0b36824378bdc08224b2f5bc43f153f083bf7
merlos <merlos@users.noreply.github.com>
2023-07-15 18:46:00 +0300

    update config.py for testing dev

--- daemon/config.py
+++ daemon/config.py
@@ -13,16 +13,19 @@ Array of ports that will be opened
 
 
 
-CERT_FILE = "/etc/openme/certificate.crt"
+#CERT_FILE = "/etc/openme/certificate.crt"
+CERT_FILE = "./openme.crt"
 """
 Certificate of the server 
 """
-KEY_FILE = "/etc/openme/private.key"
+#KEY_FILE = "/etc/openme/private.key"
+KEY_FILE = "./openme.key"
 """
 Private key of the server certificate
 """
 
-CA_CERT_FILE = "/etc/openme/ca_certificate.crt"
+#CA_CERT_FILE = "/etc/openme/ca.crt"
+CA_CERT_FILE = "./ca.crt"
 """
 Certification authority (CA) root certificate. Only clients that have connected
 to the server using a certificate issued by this authority will go through.
