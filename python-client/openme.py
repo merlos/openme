e1b9d63cc4cf972a7589dc4104f36e4fe561e77d
merlos <merlos@users.noreply.github.com>
2023-10-19 05:31:09 +0300

    add meopen to cli

--- python-client/openme.py
+++ python-client/openme.py
@@ -7,8 +7,10 @@ import config
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--server", default=config.DEFAULT_SERVER, help="Server address")
-parser.add_argument("-p", "--port", type=int, default=config.DEFAULT_PORT, help="Server port")
-parser.add_argument("-i", "--ip-address", help="Open ports to this IP address (default: your IP)")
+parser.add_argument("-p", "--port", type=int, default=config.DEFAULT_PORT, help="Server port (default:54154)")
+parser.add_argument("-i", "--ip-address", help="Open/Close ports to this IP address (default: source IP)")
+parser.add_argument("-m", "--miopen", action="store_true", default=False, help="Close ports")
+
 args = parser.parse_args()
 
 # Connect to the server using SSL
@@ -20,5 +22,17 @@ context.load_verify_locations(cafile=config.CA_CERT)
 with socket.create_connection((args.server, args.port)) as sock:
     with context.wrap_socket(sock, server_hostname=args.server) as secure_sock:
         # Send the appropriate message based on the presence of IP address
-        message = "OPEN ME" if args.ip_address is None else f"OPEN {args.ip_address}"
+        if args.miopen:
+            message = "MEOPEN" if args.ip_address is None else f"MIOPEN {args.ip_address}"
+        else: 
+            message = "OPEN ME" if args.ip_address is None else f"OPEN {args.ip_address}"
         secure_sock.sendall(message.encode())
+        
+        # Receive and handle server response
+        response = secure_sock.recv(1024).decode()
+        if response == "OK":
+            print("Server response: OK")
+        elif response == "KO":
+            print("Server response: KO (error)")
+        else:
+            print("Unexpected server response:", response)
