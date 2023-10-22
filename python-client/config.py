59eb4f04364be49b414991d533abaeab1c3c1b8e
merlos <merlos@users.noreply.github.com>
2023-10-22 05:21:04 +0300

    remove conditional in python client config

--- python-client/config.py
+++ python-client/config.py
@@ -3,14 +3,9 @@
 DEFAULT_SERVER = "localhost"
 DEFAULT_PORT = 54154 # SALSA
 
-CLIENT_CERT='/etc/openme/client.crt'
-CLIENT_KEY='/etc/openme/client.key'
-CA_CERT='/etc/openme/ca.crt'
+CLIENT_CERT='../certs/client.crt'
+CLIENT_KEY='../certs/client.key'
+CA_CERT='../certs/ca.crt'
 
 # If debug is set to True, then it uses the certs in cert
 DEBUG = True
-
-if DEBUG:
-    CLIENT_CERT='../certs/client.crt'
-    CLIENT_KEY='../certs/client.key'
-    CA_CERT='../certs/ca.crt'
