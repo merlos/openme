3394926a86bb9c347fda71038dc32eee2fd46f82
merlos <merlos@users.noreply.github.com>
2026-02-27 17:01:47 +0300

    add sdift docc plugin

--- apple/OpenMeKit/Package.swift
+++ apple/OpenMeKit/Package.swift
@@ -13,6 +13,12 @@ let package = Package(
             targets: ["OpenMeKit"]
         ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.0.0"
+        ),
+    ],
     targets: [
         .target(
             name: "OpenMeKit",
